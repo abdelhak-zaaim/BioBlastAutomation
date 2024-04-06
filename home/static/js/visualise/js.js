59e793f6eb110c14627763abfd6893c1b372db70
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-04-06 23:59:44 +0000

    commit

--- home/static/js/visualise/js.js
+++ home/static/js/visualise/js.js
@@ -1,6 +1,10 @@
 $(document).ready(async function () {
 
-    $('#sequenceTable').DataTable();
+     $('#sequenceTable').DataTable({
+        language: {
+            url: 'https://cdn.datatables.net/plug-ins/1.10.21/i18n/French.json'
+        }
+    });
 
 
 
