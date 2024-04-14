2da8e7e864dc7b165c35950fb8a1f28cfa0f9565
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-04-14 18:41:56 +0100

    Enhanced QueryRequestValidator middleware for improved efficiency. Updated the handling of sequence_string and file_fasta inputs to support both being provided in the request. Also, refined error handling for better user feedback.

--- blast/middleware/QueryRequestValidator.py
+++ blast/middleware/QueryRequestValidator.py
@@ -12,7 +12,7 @@ class QueryRequestValidator:
             sequence_string = request.POST.get('sequence_string')
             from_value = request.POST.get('from')
             to_value = request.POST.get('to')
-            file_fasta = request.FILES.get('file_fasta')
+            file_fasta = request.FILES['file_fasta']
             jtitle = request.POST.get('jtitle')
             blast_program = request.POST.get('blast_program')
             blast_database = request.POST.get('blast_database')
@@ -20,18 +20,33 @@ class QueryRequestValidator:
             if not (sequence_string or file_fasta):
                 return JsonResponse({'error': 'Invalid request'}, status=400)
 
+
+            if not blast_program or not blast_database:
+                return JsonResponse({'error': 'Invalid request'}, status=400)
+
+            if not Utils.is_valid_blast_program(blast_program):
+                return JsonResponse({'error': 'Invalid blast program'}, status=400)
+
+            if not Utils.is_valid_blast_database(blast_database):
+                return JsonResponse({'error': 'Invalid blast database'}, status=400)
+
+            if not Utils.is_program_database_compatible(blast_program, blast_database):
+                return JsonResponse({'error': 'Incompatible blast program and database'}, status=400)
+
             sequences = []
             if file_fasta:
                 try:
-                    file_fasta = file_fasta.read().decode('utf-8')
-                    sequences = Utils.validate_fasta_file(file_fasta)
+                    fasta_string = file_fasta.read().decode('utf-8')
+                    sequences_file = Utils.validate_fasta_string(fasta_string)
+                    sequences.extend(sequences_file)
                 except Exception as e:
-                    return JsonResponse({'error': str(e)}, status=400)
-            else:
+                    return JsonResponse({'error': 'Error reading file: ' + str(e)}, status=400)
+            if sequence_string:
                 try:
-                    sequences = Utils.validate_fasta_string(sequence_string)
+                    sequences_string = Utils.validate_fasta_string(sequence_string)
+                    sequences.extend(sequences_string)
                 except Exception as e:
-                    return JsonResponse({'error': str(e)}, status=400)
+                    return JsonResponse({'error': 'Error validating FASTA string: ' + str(e)}, status=400)
 
             request.data = {
                 'sequences': sequences,
@@ -42,11 +57,7 @@ class QueryRequestValidator:
                 'blast_database': blast_database
             }
 
-            if not from_value or not to_value or not blast_program or not blast_database:
-                return JsonResponse({'error': 'Invalid request'}, status=400)
 
-            if not Utils.is_valid_blast_program(blast_program):
-                return JsonResponse({'error': 'Invalid blast program'}, status=400)
 
         response = self.get_response(request)
         return response
